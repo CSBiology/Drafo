149359d9255f797f9b07ca1e39fb0f0c26374927
David Zimmer <davidzimmer91@gmail.com>
2021-04-21 08:59:13 +0200

    add unit tests

--- tests/Drafo.Tests/Sample.fs
+++ tests/Drafo.Tests/Sample.fs
@@ -1,20 +1,165 @@
 module Tests
 
 open Expecto
+open Drafo
+open Deedle
 
 [<Tests>]
 let tests =
-  testList "samples" [
-    testCase "universe exists (╭ರᴥ•́)" <| fun _ ->
-      let subject = true
-      Expect.isTrue subject "I compute, therefore I am."
-
-    testCase "I'm skipped (should skip)" <| fun _ ->
-      Tests.skiptest "Yup, waiting for a sunny day..."
+  testList "Core" [
+    let testFrame = 
+      let index1 = 
+        Series.ofValues [
+          "one"
+          "two"
+          "three"
+          "four"    
+        ]
+      let index2 = 
+        Series.ofValues [
+          1
+          2
+          3
+          4    
+        ]
+      let index3 = 
+        Series.ofValues [
+          "group 1"
+          "group 1"
+          "group 2"
+          "group 2"    
+        ]
+      let value1 = 
+        Series.ofValues [
+          1000.
+          2000.
+          nan
+          1.    
+        ]
+      let value2 = 
+        Series.ofValues [
+          10000.
+          20000.
+          nan
+          10.    
+        ]  
+      Frame.ofColumns(["index1",index1])
+      |> Frame.addCol "index2" index2
+      |> Frame.addCol "index3" index3
+      |> Frame.addCol "value1" value1
+      |> Frame.addCol "value2" value2
+    let testFrameIndexed = 
+      testFrame
+      |> Drafo.Core.indexWithColumnValues ["index1";"index2";"index3"]
     
-    testCase "contains things" <| fun _ ->
-      Expect.containsAll [| 2; 3; 4 |] [| 2; 4 |]
-                         "This is the case; {2,3,4} contains {2,4}"
+    testCase "indexing from column key list :1" <| fun _ ->
+      let k = testFrameIndexed.GetRowKeyAt (int64 1)
+      let i = k?index1 |> string 
+      Expect.equal i "two" "value sucessfully added as key property"
+
+    testCase "indexing from column key list :2" <| fun _ ->
+      let k = testFrameIndexed.GetRowKeyAt (int64 1)
+      let i = k?index2 |> string  |> int 
+      Expect.equal i 2 "value sucessfully added as key property"
+    
+    testCase "indexing from column key list :3" <| fun _ ->
+      let k = testFrameIndexed.GetRowKeyAt (int64 1)
+      let i = k?index3 |> string  
+      Expect.equal i "group 1" "value sucessfully added as key property"  
+    
+    testCase "getColumn" <| fun _ ->
+      let c =
+        testFrameIndexed
+        |> Drafo.Core.getColumn<float>"value1"
+      let check = Series.getAt 1 c = 2000.
+      Expect.isTrue check "columnKeysAreEqual" 
+
+    testCase "createFilter" <| fun _ ->
+      let c =
+        testFrameIndexed
+        |> Drafo.Core.getColumn<string>"index3"
+      let f = Drafo.Core.createFilter (fun v -> v = "group 1") c
+      let checkTrue = Series.getAt 1 f 
+      let checkFalse = Series.getAt 2 f |> not
+      Expect.isTrue (checkTrue && checkFalse) "columnKeysAreEqual" 
+    
+    testCase "aggregate_simple" <| fun _ ->
+      let v = 
+        testFrameIndexed
+        |> Drafo.Core.getColumn<float>"value1"
+      let a = Drafo.Core.aggregate (Seq.fold (fun acc x -> acc + x) 0.) ["index3"] [] v 
+      let checkTrue = Series.getAt 0 a |> (=) 3000. 
+      let checkTrue' = Series.getAt 1 a |> (=) 1.
+      Expect.isTrue (checkTrue && checkTrue') "columnKeysAreEqual" 
+
+    
+    testCase "aggregate_withFilter" <| fun _ ->
+      let c =
+        testFrameIndexed
+        |> Drafo.Core.getColumn<string>"index3"
+      let f = Drafo.Core.createFilter (fun v -> v = "group 1") c
+      let v = 
+        testFrameIndexed
+        |> Drafo.Core.getColumn<float>"value1"
+      let a = Drafo.Core.aggregate (Seq.fold (fun acc x -> acc + x) 0.) ["index3"] [f] v 
+      let checkTrue = Series.getAt 0 a |> (=) 3000. 
+      let checkTrue' = Series.countValues a |> (=) 1
+      Expect.isTrue (checkTrue && checkTrue') "columnKeysAreEqual" 
 
 
+
+    testCase "assemble_keyEqual" <| fun _ ->
+      let c =
+        testFrameIndexed
+        |> Drafo.Core.getColumn<string>"index3"
+      let f = Drafo.Core.createFilter (fun v -> v = "group 1") c
+      let v1 = 
+        testFrameIndexed
+        |> Drafo.Core.getColumn<float>"value1"
+      let a1 = Drafo.Core.aggregate (Seq.fold (fun acc x -> acc + x) 0.) ["index3"] [f] v1 
+      let v2 = 
+        testFrameIndexed
+        |> Drafo.Core.getColumn<float>"value2"
+      let a2 = Drafo.Core.aggregate (Seq.fold (fun acc x -> acc + x) 0.) ["index3"] [f] v2 
+      let assembeled = 
+        Drafo.Core.assemble 
+            [
+            "V1"  , a1 :> ISeries<Drafo.Core.Key>
+            "V2"  , a2 :> ISeries<Drafo.Core.Key>
+            ]
+      let toSave = 
+        assembeled
+      let ckEqual = 
+        Seq.map2 (fun x y -> x = y) toSave.ColumnKeys ["V1";"V2"]
+        |> Seq.contains false
+        |> not
+      Expect.isTrue (ckEqual) "columnKeysAreEqual"    
+
+    testCase "assemble_keyEqual_2" <| fun _ ->
+      let c =
+        testFrameIndexed
+        |> Drafo.Core.getColumn<string>"index3"
+      let f = Drafo.Core.createFilter (fun v -> v = "group 1") c
+      let v1 = 
+        testFrameIndexed
+        |> Drafo.Core.getColumn<float>"value1"
+      let a1 = Drafo.Core.aggregate (Seq.fold (fun acc x -> acc + x) 0.) ["index3"] [f] v1 
+      let v2 = 
+        testFrameIndexed
+        |> Drafo.Core.getColumn<float>"value2"
+      let a2 = Drafo.Core.aggregate (Seq.fold (fun acc x -> acc + x) 0.) ["index3"] [f] v2 
+      let assembeled = 
+        Drafo.Core.assemble 
+            [
+            "V1"  , a1 :> ISeries<Drafo.Core.Key>
+            "V2"  , a2 :> ISeries<Drafo.Core.Key>
+            ]
+      let toSave = 
+        assembeled
+        |> Drafo.Core.rowKeyToColumns 
+      let ckEqual = 
+        Seq.map2 (fun x y -> x = y) toSave.ColumnKeys ["index3";"V1";"V2"]
+        |> Seq.contains false
+        |> not
+      Expect.isTrue (ckEqual) "columnKeysAreEqual"                                  
   ]
