f8a9b7e2c35bf6442a2774e311a4fd9c566bdd36
David Zimmer <davidzimmer91@gmail.com>
2021-04-22 16:51:21 +0200

    add tmp code

--- src/GroupWiseNumericTransform/Library.fs
+++ src/GroupWiseNumericTransform/Library.fs
@@ -18,3 +18,43 @@ module GetColumn =
         let column = res |> getColumn<string> targetCol
         let toSave = seriesToFrame column
         toSave.SaveCsv(outFilePath,separator='\t',includeRowKeys=false)   
+// module GroupWiseNumericTransform =
+    
+//     type GroupWiseNumericTransform =
+//         | DivideByMedian
+//         | DivideByMean  
+//         | SubstractMedian
+//         | SubstractMean  
+        
+//     let initGroupWiseNumericTransform transform =
+//         match transform with 
+//         | DivideByMedian -> 
+//             fun x -> 
+//                 let m = Seq.median x
+//                 fun x -> x / m 
+//         | DivideByMean   -> 
+//                 fun x -> 
+//                     let m = Seq.mean x
+//                     fun x -> x / m
+//         | SubstractMedian   -> 
+//                 fun x -> 
+//                     let m = Seq.median x
+//                     fun x -> x - m
+//         | SubstractMean   -> 
+//                 fun x -> 
+//                     let m = Seq.mean x
+//                     fun x -> x - m
+
+//     let groupWiseNumericTransform transform (keyColumns:seq<KeyColumns.KeyColumns>) (outputDir:string) (fp:string) name =
+//         let outFilePath =
+//             let fileName = (name ) + ".value"
+//             Path.Combine [|outputDir;fileName|]
+//         let res = KeyColumns.readAndIndexFrame fp 
+//         let op = initGroupWiseNumericTransform transform
+//         let groupKeyCols = keyColumns |> Seq.map KeyColumns.initKeyCols |> Seq.map (fun x -> x.ColumnName)
+//         let column = 
+//             res 
+//             |> getColumn<float> "Value"
+//             |> groupTransform op groupKeyCols  
+//         let toSave = seriesToFrame column
+//         toSave.SaveCsv(outFilePath,separator='\t',includeRowKeys=false)   
