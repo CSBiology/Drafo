eff3c574563528ade5c9ca0ebe016f76fd480e91
David Zimmer <davidzimmer91@gmail.com>
2021-04-22 16:50:43 +0200

    add tmp code

--- src/NumericAggregation/Library.fs
+++ src/NumericAggregation/Library.fs
@@ -18,3 +18,30 @@ module GetColumn =
         let column = res |> getColumn<string> targetCol
         let toSave = seriesToFrame column
         toSave.SaveCsv(outFilePath,separator='\t',includeRowKeys=false)   
+// module NumericAggregation =
+    
+//     type NumericAggregation = 
+//         | Mean 
+//         | Median 
+//         | Sum
+
+//     let initAggregation aggregation  = 
+//         match aggregation with 
+//         | Mean      -> fun (x:seq<float>) -> Seq.mean x
+//         | Median    -> fun (x:seq<float>) -> Seq.median x
+//         | Sum       -> fun (x:seq<float>) -> Seq.sum x
+
+
+//     let aggregate aggregation (keyColumns:seq<KeyColumns.KeyColumns>) (outputDir:string) (filters:seq<string>) (fp:string) name =
+//         let outFilePath =
+//             let fileName = (name ) + ".value"
+//             Path.Combine [|outputDir;fileName|]
+//         let op = initAggregation aggregation
+//         let valueCol = KeyColumns.readAndIndexFrame fp |> getColumn<float> "Value"
+//         let filterCols = filters |> Seq.map KeyColumns.readAndIndexFrame |> Seq.map (getColumn<bool> "Value")
+//         let groupKeyCols = keyColumns |> Seq.map KeyColumns.initKeyCols |> Seq.map (fun x -> x.ColumnName)
+//         let column = 
+//             valueCol
+//             |> aggregate op groupKeyCols filterCols  
+//         let toSave = seriesToFrame column
+//         toSave.SaveCsv(outFilePath,separator='\t',includeRowKeys=false)   
