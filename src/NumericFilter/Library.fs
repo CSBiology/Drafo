51ccafadc24dae7ac0c9a0056cd840d93f5c7f15
David Zimmer <davidzimmer91@gmail.com>
2021-04-22 16:52:02 +0200

    add tmp code

--- src/NumericFilter/Library.fs
+++ src/NumericFilter/Library.fs
@@ -18,3 +18,27 @@ module GetColumn =
         let column = res |> getColumn<string> targetCol
         let toSave = seriesToFrame column
         toSave.SaveCsv(outFilePath,separator='\t',includeRowKeys=false)   
+
+// module NumericFilter =
+    
+//     type NumericFilter = 
+//         | IsBiggerThan of float 
+//         | IsSmallerThan of float 
+
+//     let initFilter filter = 
+//         match filter with 
+//         | IsBiggerThan v -> fun x -> x > v
+//         | IsSmallerThan v -> fun x -> x < v
+
+//     let numericFilter filter (outputDir:string) (fp:string) name =
+//         let outFilePath =
+//             let fileName = (name ) + ".filter"
+//             Path.Combine [|outputDir;fileName|]
+//         let res = KeyColumns.readAndIndexFrame fp 
+//         let op = initFilter filter
+//         let column = 
+//             res 
+//             |> getColumn<float> "Value"
+//             |> transform op   
+//         let toSave = seriesToFrame column
+//         toSave.SaveCsv(outFilePath,separator='\t',includeRowKeys=false)   
