e88519cfeecff6a2cb230b88ad5cfcd6c94ae842
David Zimmer <davidzimmer91@gmail.com>
2021-04-22 16:52:42 +0200

    add tmp code

--- src/NumericTransform/Library.fs
+++ src/NumericTransform/Library.fs
@@ -18,3 +18,33 @@ module GetColumn =
         let column = res |> getColumn<string> targetCol
         let toSave = seriesToFrame column
         toSave.SaveCsv(outFilePath,separator='\t',includeRowKeys=false)   
+
+module NumericTransform =
+    
+    type NumericTransform =
+        | Log2
+        | Substract of float 
+        | Add of float
+        | DivideBy of float
+        | MultiplyBy of float 
+        
+    let initNumericTransform transform =
+        match transform with 
+        | Log2          -> log2 
+        | Substract v   -> fun x -> x - v 
+        | Add v         -> fun x -> v + x
+        | DivideBy v    -> fun x -> x / v
+        | MultiplyBy v  -> fun x -> x * v 
+
+    let numericTransform numericTransform (outputDir:string) (fp:string) name =
+        let outFilePath =
+            let fileName = (name ) + ".value"
+            Path.Combine [|outputDir;fileName|]
+        let res = KeyColumns.readAndIndexFrame fp 
+        let op = initNumericTransform numericTransform
+        let column = 
+            res 
+            |> getColumn<float> "Value"
+            |> transform op   
+        let toSave = seriesToFrame column
+        toSave.SaveCsv(outFilePath,separator='\t',includeRowKeys=false)   
